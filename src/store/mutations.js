10549b008ddc28074013a9c74e5677bf25a27a1b
zhouzhihu <web_study2004@163.com>
2017-05-24 12:25:10 +0800

    增加用户信息本地存储，防止刷新后丢失

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,10 +1,22 @@
-import {
-  SET_EGRANDCONTEXT
-} from './mutation-types.js'
+import {SET_EGRANDCONTEXT, INIT_EGRANDCONTEXT, CLEAR_EGRANDCONTEXT} from './mutation-types.js'
+import {setStore,getStore, removeStore} from '../config/mUtils'
 
 export default {
   [SET_EGRANDCONTEXT](state, context){
     state.egrandContext = context;
     state.login = true;
+    //存入localStorage
+    setStore('egrandContext', state.egrandContext);
+  },
+  [INIT_EGRANDCONTEXT](state, context){
+    let egrandContext = getStore('egrandContext');
+    if (egrandContext) {
+      state.egrandContext = JSON.parse(egrandContext);
+    }
+  },
+  [CLEAR_EGRANDCONTEXT](state){
+    state.egrandContext = null;
+    state.login = false;
+    removeStore('egrandContext');
   }
 }
