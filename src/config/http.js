ff654760bbe86bd07b79ebf6ee08f08fff3aeade
littleMarkZ <1581102789@163.com>
2017-05-25 10:37:11 +0800

    还原http.js配置信息

--- src/config/http.js
+++ src/config/http.js
@@ -4,8 +4,8 @@ import store from '../store/index'
 import {CLEAR_EGRANDCONTEXT} from '../store/mutation-types'
 
 axios.defaults.timeout = 5000
-axios.defaults.baseURL = 'http://easy-mock.com/mock/59224b5b9aba4141cf29a034/szyd'
-axios.defaults.withCredentials = false
+axios.defaults.baseURL = 'http://10.1.10.16:7001'
+axios.defaults.withCredentials = true
 
 //返回状态判断(添加响应拦截器)
 axios.interceptors.response.use((res) =>{
