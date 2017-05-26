75670e8fbd6cf5011abee7e536011b6997e194de
zhouzhihu <web_study2004@163.com>
2017-05-26 15:59:40 +0800

    增加默认post的Content-Type

--- src/config/http.js
+++ src/config/http.js
@@ -2,10 +2,22 @@ import axios from 'axios'
 import router from '../router/index'
 import store from '../store/index'
 import {CLEAR_EGRANDCONTEXT} from '../store/mutation-types'
+import {getToken} from '../config/mUtils'
 
 axios.defaults.timeout = 5000
 axios.defaults.baseURL = 'http://10.1.10.16:7001'
 axios.defaults.withCredentials = true
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+
+//axios.interceptors.request.use(config => {
+//  const token = getToken("csrfToken");
+//  if (token) {
+//    config.headers["X-XSRF-TOKEN"] = token;
+//  }
+//  return config;
+//},err => {
+//  return Promise.reject(err);
+//});
 
 //返回状态判断(添加响应拦截器)
 axios.interceptors.response.use((res) =>{
