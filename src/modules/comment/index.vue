f8c9db67475ec5d82e235b360349d7e46a3f64b5
littleMarkZ <1581102789@163.com>
2017-05-26 11:35:18 +0800

    增加意见反馈页面

---
+++ src/modules/comment/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="comment">
+    <div class="page-field">
+      <div class="page-part">
+        <mt-field v-model="content" placeholder="写下您的需求和建议" type="textarea" rows="10" class="comment_font"></mt-field>
+        <mt-field v-model="contact" placeholder="联系方式（邮箱、QQ号等）" type="text" class="comment_font"></mt-field>
+      </div>
+    </div>
+    <div class="page-button">
+      <div class="page-button-group">
+        <mt-button type="primary" size="large" @click="save">提交</mt-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        content: "",
+        contact: ""
+      }
+    },
+    methods: {
+      save(evt) {
+        console.log("建议：" + this.content);
+        console.log("联系方式：" + this.contact);
+      }
+    },
+    mounted(){
+      this.$root.$emit.apply(this.$root, ['change-header'].concat(["意见反馈", true]));
+    }
+  }
+</script>
+
+<style lang="css">
+  .comment_font {
+    border-bottom: 1px solid #e6e6e6;
+  }
+  a:hover {
+    text-decoration: none;
+    color: #000;
+  }
+</style>
