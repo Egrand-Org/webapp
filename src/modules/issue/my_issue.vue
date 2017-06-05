766e914682fe524bd684fd7a094666b4f2c40298
zhouzhihu <web_study2004@163.com>
2017-06-05 12:07:17 +0800

    修改界面

--- src/modules/issue/my_issue.vue
+++ src/modules/issue/my_issue.vue
@@ -1,67 +1,60 @@
 <template>
   <div class="home">
-    <section class="sort_container">
-      <div class="sort_item">
-        <div class="sort_item_container">
-          <div class="sort_item_border">
-            <span>筛选</span>
-            <span class="glyphicon glyphicon-chevron-down sort_icon"></span>
-          </div>
+    <div class="header">
+      <div class="row item">
+        <div class="col-xs-3 active">
+          已上会
+        </div>
+        <div class="col-xs-3">
+          待上会
+        </div>
+        <div class="col-xs-3">
+          审批中
+        </div>
+        <div class="col-xs-3">
+          执行中
         </div>
       </div>
-      <div class="sort_item">
-        <div class="sort_item_container">
-          <div>
-            <span>排序</span>
-            <span class="glyphicon glyphicon-chevron-down sort_icon"></span>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section class="hy_list_container">
-      <div class="hylist_container">
+    </div>
+    <section class="list_container">
         <ul>
-          <li class="hy_li" @click="clickItem(issue.id)" v-for="issue in issues">
-            <section>
-              <div class="hy_img text-center">
-                <span class="glyphicon glyphicon-file hy_file"></span>
+          <li @click="clickItem(issue.id)" v-for="issue in issues">
+            <div class="row item">
+              <div class="col-xs-12 no-padding">
+                <hgroup>
+                  <div class="info">
+                    <span class="name">{{issue.authorName}}</span>
+                    <span class="time">{{relativeTime(issue.fileDate)}}</span>
+                  </div>
+                  <div>
+                    <p class="title">{{issue.name}}</p>
+                  </div>
+                  <div class="bottom">
+                    {{issue.ytlx}}&nbsp;&nbsp;已上会
+                  </div>
+                </hgroup>
               </div>
-            </section>
-            <hgroup class="shop_right">
-              <header class="shop_detail_header">
-                <h4 class="shop_title ellipsis">{{issue.name}}</h4>
-              </header>
-              <h5 class="fee_distance">
-                <section class="fee">
-                  议题类型：{{issue.ytlx}}
-                  &nbsp;&nbsp;
-                  发起人：{{issue.authorName}}
-                </section>
-              </h5>
-              <h5 class="fee_distance">
-                <section class="fee">
-                  创建时间：{{issue.fileDate}}
-                </section>
-              </h5>
-            </hgroup>
+            </div>
           </li>
         </ul>
-      </div>
     </section>
   </div>
 </template>
 
 <script>
   import {getPage4My} from '../../service/issue'
+  import {relativeTime} from '../../config/mUtils'
   export default {
     data() {
+      this.relativeTime = relativeTime;
       return {
-        issues : []
+        issues: []
       }
     },
     mounted(){
-      this.$root.$emit.apply(this.$root, ['change-header'].concat(["我发起的议题", true]));
+      this.$root.$emit.apply(this.$root, ['change-header'].concat(["我发起的议题", false]));
       getPage4My().then(value => {
+        console.log(value)
         this.issues = value;
       });
     },
@@ -75,105 +68,38 @@
 
 <style lang="stylus" scoped>
   .home
-    padding-top: 3.5rem;
-    width 100%
-    height 100%
-    .sort_container
-      background-color: #fff;
-      border-bottom: .025rem solid #f1f1f1;
-      position: fixed;
-      top: 1.9rem;
-      right: 0;
+    .header
+      height: 2rem;
+      line-height: 2rem;
       width: 100%;
-      display: -webkit-box;
-      display: -ms-flexbox;
-      display: flex;
-      z-index: 13;
-      box-sizing: border-box;
-      .sort_item
-        font-size: .55rem;
-        color: #444;
-        width: 50%;
-        height: 1.6rem;
-        text-align: center;
-        line-height: 1rem;
-        .sort_item_container
-          width: 100%;
-          height: 100%;
-          position: relative;
-          z-index: 14;
-          background-color: #fff;
-          box-sizing: border-box;
-          padding-top: 0.4rem;
-          .sort_item_border
-            height: 1rem;
-            border-right: .035rem solid #e4e4e4;
-            .sort_icon
-              height 0.5rem
-    .hy_list_container
-      .hylist_container
-        background-color: #fff;
-        padding-bottom: 2rem;
-      .hy_li
-        display: -webkit-box;
-        display: -ms-flexbox;
-        display: flex;
+      border-bottom: .025rem solid #f1f1f1;
+      .item
+        margin: 0px;
+        width: 100%;
+        .active
+          border-bottom: .025rem solid #26a2ff;
+    .list_container
+      li
         border-bottom: .025rem solid #f1f1f1;
         padding: 0.5rem;
-        .hy_img
+        .item
+          margin: 0px;
           width: 100%;
-          height: 100%;
-          display: block;
-          margin-right: .4rem;
-          border-right: .025rem solid #f1f1f1;
-          padding-top: 1.5rem;
-          .hy_file
-            color: #ccc;
-            font-size: 1rem;
-            .active
-              color: #3190e8;
-        .shop_right
-          -webkit-box-flex: 1;
-          -ms-flex: auto;
-          flex: auto;
-          margin-left: 0.4rem;
-          .shop_detail_header
-            display: -webkit-box;
-            display: -ms-flexbox;
-            display: flex;
-            -webkit-box-pack: justify;
-            -ms-flex-pack: justify;
-            justify-content: space-between;
-            -webkit-box-align: center;
-            -ms-flex-align: center;
-            align-items: center;
-            .shop_title
-              width: 10rem;
-              color: #333;
-              padding-top: .01rem;
-              font: .65rem/.65rem PingFangSC-Regular;
-              font-weight: 700;
-              .ellipsis
-                overflow: hidden;
-                text-overflow: ellipsis;
-                white-space: nowrap;
-          .fee_distance
-            margin-top: 1rem;
-            display: -webkit-box;
-            display: -ms-flexbox;
-            display: flex;
-            -webkit-box-pack: justify;
-            -ms-flex-pack: justify;
-            justify-content: space-between;
+          .no-padding
+            padding: 0px;
+        .info
+          .name
             font-size: .5rem;
+          .time
             color: #666;
-            .fee
-              font-size: .5rem;
-              color: #666;
-              span
-                color: #999;
-              .segmentation
-                color: #ccc;
-              .order_time
-                color: #3190e8;
+            font-size: .5rem;
+        .title
+          color: #333;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+          padding: .5rem 0 .5rem 0;
+        .bottom
+          font-size: .5rem;
+          color: #666;
 </style>
